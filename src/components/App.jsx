33edc3b72c8bf0906e05d9342ee0b2e873bad9be
Jorge Fernandez <jorgelfc1989@gmail.com>
2022-07-20 20:46:04 -0400

    made small changes to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,6 @@ import React from "react";
 import emojipedia from "../emojipedia";
 import Card from "./Card";
 
-function createCard(emoj){
-  return(
- <Card
- key ={emoj.id}
-emoji={emoj.emoji}
-name={emoj.name}
-meaning = {emoj.meaning}
-  />
-  )
-}
-
 function App() {
   return (
     <div>
@@ -20,7 +9,15 @@ function App() {
         <span>emojipedia</span>
       </h1>
       <dl className="dictionary">
-          {emojipedia.map(createCard)}
+        {emojipedia.map(ele => {
+          return (<Card
+            key={ele.id}
+            emoji={ele.emoji}
+            name={ele.name}
+            meaning={ele.meaning}
+          />
+          )
+        })}
       </dl>
     </div>
   );
